a7f9f8ca8c205ce486bb147617f81149d97d4fa1
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-03-03 17:11:38 -0300

    add static files, static dirs (settings.py), templates files, templater dirs, partials, urls and views files in contacs

--- ScheduleDjango/settings.py
+++ ScheduleDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Contacts.apps.ContactConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ScheduleDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILE_DIRS = [
+    BASE_DIR / 'templates/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

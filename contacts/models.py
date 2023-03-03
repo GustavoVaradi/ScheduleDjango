73fc42b2508feca99ae2aac705395de163f56571
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-03-03 17:50:40 -0300

    created the models, database for app

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Contacs(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=20)
+    mail = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)

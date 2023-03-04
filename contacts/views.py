f0b27aa848980c0abf07eb1a2179d115b6fd64d0
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-03-04 15:01:15 -0300

    added a error 404

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contacts
 
 # Create your views here.
@@ -6,4 +7,12 @@ def index(request):
     contacts = Contacts.objects.all() # semelhante a consulta all em SQL
     return render(request, 'contacts/index.html', {
         'contacts': contacts
+    })
+
+def show_contact(request, contact_id):
+   
+    # contact = Contacts.objects.get(id = contact_id)
+    contact = get_object_or_404(Contacts, id=contact_id)
+    return render(request, 'contacts/show_contact.html', {
+        'contact': contact
     })

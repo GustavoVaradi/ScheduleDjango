9c653baf5308a433594c75af4986abc1009aae64
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-03-04 23:07:39 -0300

    created a lading page

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,7 +4,7 @@ from .models import Contacts, Category
 # Register your models here.
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'surname', 'phone', 'mail', 'creation_date')
+    list_display = ('id', 'name', 'surname', 'phone', 'mail', 'creation_date', 'show')
 
     list_display_links = ('id', 'name', 'surname')
 
@@ -13,6 +13,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
 
     search_fields = ('name', 'id', 'creation_date')
+
+    list_editable = ('phone', 'show')
 
     
 
--- contacts/views.py
+++ contacts/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Contacts
 
 # Create your views here.
 def index(request):
     contacts = Contacts.objects.all() # semelhante a consulta all em SQL
-    return render(request, 'contacts/index.html', {
-        'contacts': contacts
-    })
+    paginator = Paginator(contacts, 2)
+
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
+
+    return render(request, 'contacts/index.html', { 'contacts': contacts })
 
 def show_contact(request, contact_id):
    
     # contact = Contacts.objects.get(id = contact_id)
     contact = get_object_or_404(Contacts, id=contact_id)
+
     return render(request, 'contacts/show_contact.html', {
         'contact': contact
     })
